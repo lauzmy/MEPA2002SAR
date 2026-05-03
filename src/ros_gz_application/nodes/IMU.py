b2248c00ebd0c9312c7be93201e4ebe9848144b0
Groven04 <sindg04@gmail.com>
2026-05-03 20:04:37 +0200

    endring av imu node

--- src/ros_gz_application/nodes/IMU.py
+++ src/ros_gz_application/nodes/IMU.py
@@ -32,46 +32,46 @@ class BNO085Node(Node):
         
         # 1. Header (Time and TF Frame)
         msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = 'imu_link' # Has to match URDF / robot state publisher
+        msg.header.frame_id = 'imu_link'
 
         try:
-            # 2. Orientation (Quaternion)
+            # 2. Hent all dataene
             quat = self.sensor.quaternion
-            # Did we acctual get valid data?
-            if quat:
+            gyro = self.sensor.gyro
+            accel = self.sensor.acceleration
+
+            # 3. KUN Publiser hvis vi faktisk har gyldig data fra sensoren
+            if quat and gyro and accel:
                 msg.orientation.x = quat[0]
                 msg.orientation.y = quat[1]
                 msg.orientation.z = quat[2]
                 msg.orientation.w = quat[3]
 
-            # 3. Gyro (angular velocity in rad/s)
-            gyro = self.sensor.gyro
-            if gyro:
                 msg.angular_velocity.x = gyro[0]
                 msg.angular_velocity.y = gyro[1]
                 msg.angular_velocity.z = gyro[2]
 
-            # 4. Accelerometer (linear acceleration in m/s^2)
-            accel = self.sensor.acceleration
-            if accel:
                 msg.linear_acceleration.x = accel[0]
                 msg.linear_acceleration.y = accel[1]
                 msg.linear_acceleration.z = accel[2]
 
-            # Sett små covariances så EKF matrisen ikke kræsjer pga null
-            msg.orientation_covariance[0] = 0.01
-            msg.orientation_covariance[4] = 0.01
-            msg.orientation_covariance[8] = 0.01
-            
-            msg.angular_velocity_covariance[0] = 0.01
-            msg.angular_velocity_covariance[4] = 0.01
-            msg.angular_velocity_covariance[8] = 0.01
-            
-            msg.linear_acceleration_covariance[0] = 0.01
-            msg.linear_acceleration_covariance[4] = 0.01
-            msg.linear_acceleration_covariance[8] = 0.01
+                # Sett små covariances så EKF matrisen ikke kræsjer
+                msg.orientation_covariance[0] = 0.01
+                msg.orientation_covariance[4] = 0.01
+                msg.orientation_covariance[8] = 0.01
+                
+                msg.angular_velocity_covariance[0] = 0.01
+                msg.angular_velocity_covariance[4] = 0.01
+                msg.angular_velocity_covariance[8] = 0.01
+                
+                msg.linear_acceleration_covariance[0] = 0.01
+                msg.linear_acceleration_covariance[4] = 0.01
+                msg.linear_acceleration_covariance[8] = 0.01
 
-            self.publisher.publish(msg)
+                self.publisher.publish(msg)
+            else:
+                # Forteller oss at sensoren fremdeles varmer opp
+                pass
             
         except Exception as e:
             self.get_logger().warn(f"Wrong reading from IMU: {e}")
