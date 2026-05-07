c182546c62ca9812a45fb8291ed6008b1361a756
Groven04 <sindg04@gmail.com>
2026-05-07 10:44:06 +0200

    deaktiverte theral nodene

--- src/ros_gz_bringup/launch/gregor.launch.py
+++ src/ros_gz_bringup/launch/gregor.launch.py
@@ -232,8 +232,6 @@ def generate_launch_description():
         IMU_node,
         allocator_node,
         ldlidar_node,
-        thermal_converter,
-        thermal_processor,
         ekf_node,
         slam,
         nav2,
